4a2d22cb83214500390d16d3105de0e1409fb1cb
Ilija <Ilija@Ilijas-Mac.local>
2014-11-29 12:33:16 -0800

    first rev of projects

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Ilija on 11/29/14.
+//  Copyright (c) 2014 Ilija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameText: UITextField!
+    @IBOutlet weak var enterMessageText: UITextField!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailBtnClick(sender: UIButton) {
+        messageLabel.hidden = false;
+        messageLabel.text = enterNameText.text + ", you sent the message!";
+        messageLabel.textColor = UIColor.redColor();
+        sender.enabled = false;
+        sendBtn.setTitle("No more cliks", forState: UIControlState.Normal)
+        enterNameText.enabled = false;
+        enterMessageText.enabled = false;
+        enterMessageText.resignFirstResponder();
+    }
+
+}
+
